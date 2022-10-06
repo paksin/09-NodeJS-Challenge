3be4549e503edd97d631c51c14255dbb0d0f6d1f
Pak Yin Sin <Spy@MacBook-Pro.local>
2022-10-06 00:48:30 -0400

    Full function code

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-{"title":"FindFlix","motive":"Trying to help people find their perfect movie. ","why":"Spread the love of movie to others. ","solved":"Saturday entertainment.","lesson":"Incorporate JavaScript and APIs to retrieve search result.","special":"UX and UI oriented"}
+
+![License](https://img.shields.io/badge/license-Unlicense-blue.svg)
+
+# Findflix 
+https://github.com/paksin/Findflix
+
+## Description    
+
+A movie information site for users that to find out more about films and favourite them. In the description, there will be a short plot, movie ratings, youtube player with that film’s trailer, and more.
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[Credits](#credits)
+
+[License](#license)
+
+## Installation
+
+Just open the deployed URL here: https://keysbhag.github.io/FindFlix-Project
+
+## Usage 
+
+Anyone
+
+## Contributing
+
+Anyone
+
+## Tests
+
+Feel free to fork on GitHub and test.
+
+## License
+
+This project is licensed under the Other license.
+
+## Questions
+
+If you have any inquiries about my repository, please feel free to reach out through GitHub or email: sinpakyin@gmail.com
+
+https://github.com/paksin
+    
--- index.js
+++ index.js
@@ -6,56 +6,135 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [
     'What is the project title?',
-    'What was your motivation?',
-    'Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")',
-    'What problem does it solve?',
-    'What did you learn?',
-    'What makes your project stand out?'
+    'Provide a short description explaining the what, why, and how of your project.',
+    'Installation instructions?',
+    'Usage information?',
+    'Contribution guidelines?',
+    'Test instructions?',
+    'Licenses? (e.g. MIT etc.)',
+    'Enter your GitHub username for questions: ',
+    'Enter your email for inquiries: '
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, `${JSON.stringify(data)}`, 
-    (err) => err ? console.error(err) : console.log('Success!'))
+    var licenseBadge;
+    switch (data.license) {
+        case 'MIT':
+            licenseBadge = "https://img.shields.io/badge/License-MIT-yellow.svg";
+            break;
+        case 'GPLv2':
+            licenseBadge = "https://img.shields.io/badge/License-GPL_v2-blue.svg";
+            break;
+        case 'Apache':
+            licenseBadge = "https://img.shields.io/badge/License-Apache_2.0-blue.svg";
+            break;
+        case 'GPLv3':
+            licenseBadge = "https://img.shields.io/badge/License-GPLv3-blue.svg";
+            break;
+        default:
+            licenseBadge = "https://img.shields.io/badge/license-Unlicense-blue.svg"
+            break;
+    }
+    fs.writeFile(fileName,
+        `
+![License](${licenseBadge})
+
+# ${data.title} 
+https://github.com/${data.username}/${data.title}
+
+## Description    
+
+${data.description}
+
+[Installation](#installation)
+
+[Usage](#usage)
+
+[Credits](#credits)
+
+[License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage 
+
+${data.usage}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## License
+
+This project is licensed under the ${data.license} license.
+
+## Questions
+
+If you have any inquiries about my repository, please feel free to reach out through GitHub or email: ${data.email}
+
+https://github.com/${data.username}
+    `, (err) => err ? console.error(err) : console.log('Success!'))
 }
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: questions[0],
-            name: 'title',
-        },
-        {
-            type: 'input',
-            message: questions[1],
-            name: 'motive',
-        },
-        {
-            type: 'input',
-            message: questions[2],
-            name: 'why',
-        },
-        {
-            type: 'input',
-            message: questions[3],
-            name: 'solved',
-        },
-        {
-            type: 'input',
-            message: questions[4],
-            name: 'lesson',
-        },
-        {
-            type: 'input',
-            message: questions[5],
-            name: 'special',
-        },
-    ])
-    .then((response) =>
-        writeToFile("README.md", response))
+        .prompt([{
+                type: 'input',
+                message: questions[0],
+                name: 'title',
+            },
+            {
+                type: 'input',
+                message: questions[1],
+                name: 'description',
+            },
+            {
+                type: 'input',
+                message: questions[2],
+                name: 'installation',
+            },
+            {
+                type: 'input',
+                message: questions[3],
+                name: 'usage',
+            },
+            {
+                type: 'input',
+                message: questions[4],
+                name: 'contributing',
+            },
+            {
+                type: 'input',
+                message: questions[5],
+                name: 'tests',
+            },
+            {
+                type: 'list',
+                message: questions[6],
+                name: 'license',
+                choices: ['MIT', 'GPLv2', 'Apache', 'GPLv3', 'Other', ]
+            },
+            {
+                type: 'input',
+                message: questions[7],
+                name: 'username',
+            },
+            {
+                type: 'input',
+                message: questions[8],
+                name: 'email',
+            },
+        ])
+        .then((response) =>
+            writeToFile("README.md", response))
 }
 
 // Function call to initialize app
